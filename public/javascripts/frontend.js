270cbc1db497c5b62cdb576168ff40bee731cb34
raba930 <rabuzin.antonio@gmail.com>
2017-11-21 22:23:15 +0100

    graph test

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -1,76 +1,83 @@
-var chartData = [{
-    "country": "USA",
-    "visits": 4252
-}, {
-    "country": "China",
-    "visits": 1882
-}, {
-    "country": "Japan",
-    "visits": 1809
-}, {
-    "country": "Germany",
-    "visits": 1322
-}, {
-    "country": "UK",
-    "visits": 1122
-}, {
-    "country": "France",
-    "visits": 1114
-}, {
-    "country": "India",
-    "visits": 984
-}, {
-    "country": "Spain",
-    "visits": 711
-}, {
-    "country": "Netherlands",
-    "visits": 665
-}, {
-    "country": "Russia",
-    "visits": 580
-}, {
-    "country": "South Korea",
-    "visits": 443
-}, {
-    "country": "Canada",
-    "visits": 441
-}, {
-    "country": "Brazil",
-    "visits": 395
-}, {
-    "country": "Italy",
-    "visits": 386
-}, {
-    "country": "Australia",
-    "visits": 384
-}, {
-    "country": "Taiwan",
-    "visits": 338
-}, {
-    "country": "Poland",
-    "visits": 328
-}];
-
-window.onload = () => {
-
-    AmCharts.makeChart("chart", {
-        "type": "serial",
-        "dataProvider": chartData,
-        "categoryField": "country",
-        "graphs": [{
-            "valueField": "visits",
-            "type": "column"
-        }]
-    });
-    console.log('LOADDDDDD');
+var Graph;
+window.onload = function() {
+    setInterval(function() {
+        getData();
+    }, 120000);
+    getData(true);
 }
 
-setInterval(() => {
+function getData(firstRender) {
     $.ajax('/current')
-        .done(res => {
+        .done(function(res) {
             if (res.entries && res.entries.length) {
-                $('#nutra span').text(res.entries[0].inside);
-                $('#vani span').text(res.entries[0].outside);
+                if (firstRender) {
+                    Graph = AmCharts.makeChart("chart", graphOptions(res.entries));
+                } else {
+                    Graph.dataProvider = res.entries;
+                }
+                $('#unutra span').text(res.entries[0].inside + ' °');
+                $('#vani span').text(res.entries[0].outside + ' °');
             }
         });
-}, 120000);
+}
+
+function graphOptions(data) {
+    return {
+        'type': 'serial',
+        'theme': 'light',
+        'autoMarginOffset': 20,
+        'addClassNames': true,
+        'dataDateFormat': 'YYYY-MM-DD HH:NN',
+        'dataProvider': data,
+        'valueAxes': [{
+            'axisAlpha': 0,
+            'position': 'left',
+            'tickLength': 0
+        }],
+        'graphs': [{
+            'balloonText': '[[category]]<br><b><span style="font-size:14px;">temp: [[value]]°</span></b>',
+            'bullet': 'round',
+            'bulletSize': 6,
+            'bulletBorderAlpha': 1,
+            'bulletColor': '#FFFFFF',
+            'lineColor': '#bb1c47',
+            'useLineColorForBulletBorder': true,
+            'bulletBorderThickness': 2,
+            'lineThickness': 3,
+            'colorField': 'color',
+            'valueField': 'outside'
+        }, {
+            'balloonText': '[[category]]<br><b><span style="font-size:14px;">temp: [[value]]°</span></b>',
+            'bullet': 'round',
+            'bulletSize': 6,
+            'bulletBorderAlpha': 1,
+            'bulletColor': '#FFFFFF',
+            'lineColor': '#67d219',
+            'useLineColorForBulletBorder': true,
+            'bulletBorderThickness': 2,
+            'lineThickness': 3,
+            'colorField': 'color',
+            'valueField': 'inside'
+        }],
+        'chartCursor': {
+            'zoomable': false,
+            'fullWidth': true,
+            'categoryBalloonDateFormat': 'YYYY-MM-DD HH:NN',
+            'valueLineEabled': true,
+            'valueLineBalloonEnabled': true,
+            'valueLineAlpha': 0.5,
+            'cursorAlpha': 0
+        },
+        'categoryField': 'created_at',
+        'startDuration': 1,
+        'categoryAxis': {
+            'parseDates': true,
+            'minPeriod': 'mm',
+            'equalSpacing': true,
+            'axisAlpha': 0,
+            'gridAlpha': 0.1,
+            'minorGridAlpha': 0.1,
+            'minorGridEnabled': true
+        }
+    };
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index');
 });
 router.get('/current', function (req, res) {
     Entry
